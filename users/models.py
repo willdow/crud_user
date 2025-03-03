163dec9058b55849802cc49ff552fa1ce877ddee
Willyan <willyan.lin@talan.com>
2025-03-03 00:29:31 -0500

    import django user model and add custom fields

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    is_eating_healthy = models.BooleanField(default=False)
+    is_playing_sport = models.BooleanField(default=False)
+    is_intellectually_stimulated = models.BooleanField(default=False)
+    
+    
