f65d28f847c8414fb86dce210ac8afb9e513f6be
Willyan <willyan.lin@talan.com>
2025-03-03 11:08:10 -0500

    add template

--- crud_user/settings.py
+++ crud_user/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'crud_user.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
